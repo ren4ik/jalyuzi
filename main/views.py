7400ec1f24a8c96e86888cb55b1847b1742c073d
ren4ik <mailren82@gmail.com>
2021-06-12 15:27:48 +0500

    start for project

--- main/views.py
+++ main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 
 class HomePage(View):
     pass
+
+
+class AboutPage(TemplateView):
+    pass
+
+
+class ContactPage(View):
+    pass
+
