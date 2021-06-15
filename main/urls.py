7874fe5d4a6e781aac4c5740edb3f4d56e500380
ren4ik <mailren82@gmail.com>
2021-06-15 18:48:46 +0500

    project settings app & models done

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from main.views import AboutPage, ContactPage
 
 urlpatterns = [
+    path('about/', AboutPage.as_view(), name="about"),
+    path('contact/', ContactPage.as_view(), name="contact")
 ]
