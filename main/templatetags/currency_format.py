29b60609fb0d43ee1b4d24b9952bf1832b1380a8
ren4ik <mailren82@gmail.com>
2021-06-19 19:02:24 +0500

    next step 4

--- main/templatetags/currency_format.py
+++ main/templatetags/currency_format.py
@@ -11,8 +11,8 @@ def intspace(value):
         For example, 3000 becomes '3 000' and 45000 becomes '45 000'.
         See django.contrib.humanize app
     """
-    new = re.sub("^(-?\d+)(\d{3})", '\g<1> \g<2>', value)
+    new = '{0:,}'.format(value).replace(',', ' ')
     if value == new:
-        return new
+        return value
     else:
-        return intspace(new)
+        return new
